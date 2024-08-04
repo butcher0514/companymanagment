e716cce37dcdaed4bf2195dd7d63ac37276ad7ec
GiThon-Won <122207698+butcher0514@users.noreply.github.com>
2024-08-04 15:46:21 +0800

    Add files via upload

---
+++ app/common.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 应用公共文件，以下常量请勿随意修改
+ * LightPicture [ 图床 ]
+ * Github: https://github.com/osuuu/LightPicture
+ */
+
+
+
+// 程序版本
+define("VERSION", "1.2.2");
+// 版本号
+define("RELRAASE_TIME", "20220227");
+// 本地存储根目录
+define("FOLDER", "LightPicture/");
+// TOKEN KEY
+define("TokenKey", "e06b7b61978b30fd1e87723ef61746fb"); 
+// 程序根目录
+define("DIR", "../");
+// 工作区目录
+define("WORKSPACE", "../runtime/tmp/");
+// 版本更新接口
+define("UPDATE_URL", "http://api.eemu.cn/LightPicture.php");
+
+
+
---
+++ app/sql/update.sql
@@ -0,0 +1,7 @@
+--
+-- Sql更新文件
+--
+
+-- v1.1
+-- 添加字段索引
+ALTER TABLE osuu_system ADD UNIQUE (`key`);
